1837e8c2b32446bfac1ec579e52b7de9ae01ec24
Petter Mansson <petter.mansson1@gmail.com>
2021-11-20 16:18:53 +0100

    Bezier mouth wont draw

--- sketch.js
+++ sketch.js
@@ -13,10 +13,14 @@ await setupAnalyzer();
 playSample(context, buffer, 0);
 
 streamAudioFromPc();
-const canvas = document.getElementById("tutorial");
-const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const spectrumCanvas = document.getElementById("spectrum");
+const spectrumCtx = spectrumCanvas.getContext("2d");
+const patternCanvas = document.getElementById("pattern");
+const patternCtx = patternCanvas.getContext("2d");
+spectrumCanvas.width = window.innerWidth;
+spectrumCanvas.height = window.innerHeight / 2;
+patternCanvas.width = window.innerWidth;
+patternCanvas.height = window.innerHeight / 2;
 
 async function getFile(audioContext, filepath) {
   const response = await fetch(filepath);
@@ -64,7 +68,7 @@ let xCirclePos = 0;
 let xCirclePosD = 100;
 
 function step() {
-  if (xCirclePos > canvas.width - 400) {
+  if (xCirclePos > spectrumCanvas.width - 400) {
     xCirclePosD = -Math.abs(xCirclePosD);
   }
   if (xCirclePos < 0) {
@@ -76,7 +80,8 @@ function step() {
 let BPMSrunning = false;
 
 const tick = () => {
-  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  spectrumCtx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  patternCtx.clearRect(0, 0, window.innerWidth, window.innerHeight);
   dataArrayFrequency = new Uint8Array(analyzer.frequencyBinCount);
   dataArrayTimeDomain = new Uint8Array(analyzer.frequencyBinCount);
   analyzer.getByteFrequencyData(dataArrayFrequency);
@@ -96,14 +101,14 @@ const tick = () => {
   ampCircle = Math.floor(ampCircle);
   ampCircle = ampCircle > circleDecay ? ampCircle : circleDecay;
 
-  ctx.lineWidth = 10;
-  ctx.strokeStyle = `rgb(${ampCircle / 3}, ${0}, ${ampCircle / 2})`;
-  ctx.beginPath();
-  ctx.arc(xCirclePos + 200, 200, ampCircle / 30, 0, 2 * Math.PI);
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.arc(xCirclePos + 400, 200, ampCircle / 30, 0, 2 * Math.PI);
-  ctx.stroke();
+  patternCtx.lineWidth = 10;
+  patternCtx.strokeStyle = `rgb(${ampCircle / 3}, ${0}, ${ampCircle / 2})`;
+  patternCtx.beginPath();
+  patternCtx.arc(xCirclePos + 200, 200, ampCircle / 30, 0, 2 * Math.PI);
+  patternCtx.stroke();
+  patternCtx.beginPath();
+  patternCtx.arc(xCirclePos + 400, 200, ampCircle / 30, 0, 2 * Math.PI);
+  patternCtx.stroke();
 
   circleDecay = circleDecay < 0 ? 0 : ampCircle - 20;
 
@@ -111,10 +116,10 @@ const tick = () => {
     BPMSrunning = true;
     const intervalID = setInterval(step, localStorage.getItem("BPMS"));
   }
-  ctx.beginPath(); // Start a new path
-  ctx.moveTo(xCirclePos + 20, 400); // Move the pen to (30, 50)
-  ctx.lineTo(xCirclePos + 600, 400); // Draw a line to (150, 100)
-  ctx.stroke();
+  patternCtx.beginPath(); // Start a new path
+  patternCtx.moveTo(xCirclePos + 20, 400); // Move the pen to (30, 50)
+  patternCtx.lineTo(xCirclePos + 600, 400); // Draw a line to (150, 100)
+  patternCtx.stroke();
 
   let bezierCurveX =
     dataArrayTimeDomain[360] < 100 ? dataArrayTimeDomain[349] : 0;
@@ -122,14 +127,14 @@ const tick = () => {
     dataArrayTimeDomain[360] < 100 ? dataArrayTimeDomain[300] : 0;
   // Define the points as {x, y}
   let start = { x: 20, y: 400 };
-  let cp1 = { x: bezierCurveX + 250, y: bezierCurveY + 450 };
-  let cp2 = { x: bezierCurveX + 250, y: bezierCurveY + 450 };
+  let cp1 = { x: bezierCurveX + 250, y: bezierCurveY + 250 };
+  let cp2 = { x: bezierCurveX + 250, y: bezierCurveY + 250 };
   let end = { x: 600, y: 400 };
 
   // Cubic Bézier curve
-  ctx.beginPath();
-  ctx.moveTo(xCirclePos + start.x, start.y);
-  ctx.bezierCurveTo(
+  patternCtx.beginPath();
+  patternCtx.moveTo(xCirclePos + start.x, start.y);
+  patternCtx.bezierCurveTo(
     xCirclePos + cp1.x,
     cp1.y,
     xCirclePos + cp2.x,
@@ -137,11 +142,15 @@ const tick = () => {
     xCirclePos + end.x,
     end.y
   );
-  ctx.stroke();
+
+  //Spectrum
+  spectrumCtx.beginPath();
+  spectrumCtx.stroke();
+  spectrumCtx.lineWidth = 10;
   dataArrayFrequency.forEach(() => {
     if (i % 10 === 0 || i === 0) {
-      ctx.fillStyle = `rgb(${amp / 4}, ${0}, ${amp + 20})`;
-      ctx.fillRect(x, window.innerHeight, 30, -amp / 5);
+      spectrumCtx.fillStyle = `rgb(${amp / 4}, ${0}, ${amp + 20})`;
+      spectrumCtx.fillRect(x, window.innerHeight/2, 30, -amp / 5);
       amp = 0;
       x += 35;
       y += 10;
